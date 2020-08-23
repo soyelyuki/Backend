3f71371f64ef64e201b28783234d66196f05ebc4
Nicolas Miranda Meza <nicolas@pop-os.localdomain>
2020-08-22 20:25:46 -0400

    Backend

--- Cursos/Backend/index.js
+++ Cursos/Backend/index.js
@@ -8,4 +8,5 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/api_rest_blog',{ useNewUrlParser:true, useUnifiedTopology: true})
     .then(() =>{
         console.log('la conexion a la base de datos se ha realizado laraja!!!');
-    });
+    });asdasd
+    
